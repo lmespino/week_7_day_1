13b1cd84068e631eeb2b0f0beeead32a3e146f86
lmespino <lmespino@cps.edu>
2024-10-07 15:12:19 -0500

    challenge 3

---
+++ challenge_3.py
@@ -0,0 +1,4 @@
+user_text = input("Give a text/article/paragraph/sentence/ or poem: ") # asks the user for text
+random_letters = input("Give three random letters: ") # asks the user again for three letters
+
+letters_list = list(random_letters.split(" "))
